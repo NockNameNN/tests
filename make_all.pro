8b14c21e2acb8bb10e787bfce20c737dd97c3220
NockNameNN <nockname2@mail.ru>
2023-12-08 15:24:02 +0700

    task three

--- make_all.pro
+++ make_all.pro
@@ -1,4 +1,5 @@
 TEMPLATE = subdirs
 SUBDIRS = \
   src/bigint.pro \
-  tests/test_bigint.pro
+  tests/test_bigint.pro \
+  tests/ui_testform
--- src/form.h
+++ src/form.h
@@ -2,6 +2,9 @@
 #define FORM_H
 
 #include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
 
 namespace Ui {
 class Form;
@@ -14,6 +17,11 @@ class Form : public QWidget
 public:
     explicit Form(QWidget *parent = nullptr);
     ~Form();
+
+    QLineEdit* getTextLineEdit() const;
+    QLineEdit* getSubStringLineEdit() const;
+    QPushButton* getSearchButton() const;
+    QLabel* getResultLabel() const;
 
 public slots:
     void start();
